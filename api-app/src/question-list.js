f124c38b9eea1b5d856bf88842c6c58703f938d9
Andrew Seaman <andrew.seaman@mullenmail.com>
2020-02-25 20:52:37 -0800

    Added a question list and am working on rendering questions

---
+++ api-app/src/question-list.js
@@ -0,0 +1,9 @@
+let questionList = [
+  {
+    question = "What word describes you the most",
+    choice1 = "Smart",
+    choice2 = "Funny",
+    choice3 = "Strong",
+    choice4 = "Serious"
+   }
+]
---
+++ api-app/src/questionLoader.js
@@ -0,0 +1,5 @@
+import React from 'react';
+// import questionList from '/Users/andrewseaman/API-Project/api-app/src/question-loader.js';
+
+const loadQuestion = <h1>HERE I AM</h1>
+export default loadQuestion;
