7a78852eb6cb6f31284eb09ba2b92a44c446f949
Maya Dahlke <mayadahlke@yahoo.com>
2020-02-28 13:01:23 -0800

    the second button works - update api

--- api-app/src/App.js
+++ api-app/src/App.js
@@ -5,8 +5,31 @@ import Navbar from './components/Navbar'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Characters from './components/Characters'
+import { useState, useEffect } from "react";
+import md5 from "md5";
 
 function App() {
+  //debugger;
+  const TS = "Date.now()";
+  const PUBLIC_KEY = "9ac99174c6637a3dd38851c3d6b0d5b1";
+  const PRIVATE_KEY = "000a54a0d6a07663808c5278ed1c58f7c9c38bad";
+  const HASH = md5(TS + PRIVATE_KEY + PUBLIC_KEY);
+
+  const [chars, setChar] = useState([]);
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  const getCharacters = async () => {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters?name=Squirrel%20Girl&ts=${TS}&apikey=${PUBLIC_KEY}&hash=${HASH}`
+    );
+    const data = await response.json();
+    setChar(data.data.results);
+    console.log(data.data.results);
+  }
+
   return (
     //loadQuestion
     <BrowserRouter>
@@ -14,9 +37,19 @@ function App() {
          <Navbar />
          <Route path='/home' component={Home} />
          <Route path='/characters' component={Characters} />
+         {chars.map(char => (
+          <Characters 
+            name={char.name}
+            description={char.description}
+          />
+        ))};
        </div>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
+
+
+
+
--- api-app/src/components/Characters.js
+++ api-app/src/components/Characters.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 const Characters = ({name, description}) => {
+    // function printData({name, description}) {
+    //     <p>{name}</p>;
+    //     <p>{description}</p>;
+    // }
+
     return (
         <div className="container">
             <h4 className="center"> Characters </h4>
             <p> More characters to Explore: </p>
             <button onClick={() => {
-                alert({name});
+                console.log({name});
                 console.log({description});
             }}>Squirrel Girl</button>
         </div>
