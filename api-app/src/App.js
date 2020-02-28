b2e434a7261000cb03c812b6bb46b7803687230a
Tanya Nobal <tanyanobal@gmail.com>
2020-02-27 18:44:37 -0800

    adds navbar to App.js

--- api-app/src/App.js
+++ api-app/src/App.js
@@ -5,6 +5,13 @@ import loadQuestion from './questionLoader';
 function App() {
   return (
     loadQuestion
+    <BrowserRouter>
+       <div className="App">
+         <Navbar />
+         <Route path='/home' component={Home} />
+         <Route path='/characters' component={Characters} />
+       </div>
+    </BrowserRouter>
   );
 }
 
