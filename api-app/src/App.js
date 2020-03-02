dc4a413f1ec5a0dcf62b54ad4b56403c964e9952
Maya Dahlke <mayadahlke@yahoo.com>
2020-03-01 17:06:35 -0800

    working api

--- api-app/src/App.js
+++ api-app/src/App.js
@@ -9,25 +9,46 @@ import { useState, useEffect } from "react";
 import md5 from "md5";
 
 function App() {
-  //debugger;
+  
   const TS = "Date.now()";
   const PUBLIC_KEY = "9ac99174c6637a3dd38851c3d6b0d5b1";
   const PRIVATE_KEY = "000a54a0d6a07663808c5278ed1c58f7c9c38bad";
   const HASH = md5(TS + PRIVATE_KEY + PUBLIC_KEY);
-
-  const [chars, setChar] = useState([]);
+  const [charactersState, setCharactersState] = useState([]);
+  const [query, setQuery] = useState('Ancient One');
+  const [listNames, setListNames] = useState({
+    charNames: [
+      'Squirrel Girl', 
+      'Ancient One', 
+      'Grandmaster',
+      'Silver Surfer',
+      'Cannonball',
+      'Machine Man',
+      'Silver Surfer',
+      'Apocalypse',
+      'Elektra',
+      'Firelord',
+      'Blade',
+      'Echo',
+      'Domino'
+    ]
+  });
 
   useEffect(() => {
     getCharacters();
-  }, []);
+  }, [query]);
 
   const getCharacters = async () => {
-    const response = await fetch(
-      `https://gateway.marvel.com/v1/public/characters?name=Squirrel%20Girl&ts=${TS}&apikey=${PUBLIC_KEY}&hash=${HASH}`
+    const response = await fetch (
+      `https://gateway.marvel.com/v1/public/characters?name=${query}&ts=${TS}&apikey=${PUBLIC_KEY}&hash=${HASH}`
     );
     const data = await response.json();
-    setChar(data.data.results);
-    console.log(data.data.results);
+    setCharactersState(data.data.results[0]);
+  }
+
+  const updateSetQueryState = e => {
+    e.preventDefault();
+    setQuery(e.target.innerText);
   }
 
   return (
@@ -36,15 +57,20 @@ function App() {
       <div className="App">
         <Navbar />
         <Route path='/home' component={Home} />
-        <Route path='/characters' component={Characters}> 
-          {chars.map(char => (
-            <Characters 
-              name={char.name}
-              description={char.description}
-            />
+        <Route path='/characters' component={Characters}>
+          <h4 className="center"> Characters </h4>
+          <p>More characters to Explore:</p>
+          {listNames.charNames.map(charName => (
+              <button onClick={updateSetQueryState}>
+                  <p>{charName}</p>
+              </button>
           ))}
+          <Characters
+            name={charactersState.name}
+            description={charactersState.description}
+          />
         </Route>
-       </div>
+        </div>
     </BrowserRouter>
   )
 }
--- api-app/src/components/Characters.js
+++ api-app/src/components/Characters.js
@@ -1,22 +1,13 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+//import { render } from '@testing-library/react'
 
-const Characters = ({name, description}) => {
-    // function printData({name, description}) {
-    //     <p>{name}</p>;
-    //     <p>{description}</p>;
-    // }
-
+const Characters = (props) => {
     return (
         <div className="container">
-            <h4 className="center"> Characters </h4>
-            <p> More characters to Explore: </p>
-            <button onClick={() => {
-                console.log({name});
-                console.log({description});
-            }}>Squirrel Girl</button>
+            <p>{props.name}</p>
+            <p>{props.description}</p>
         </div>
-    );
+    );       
 }
 
 export default Characters
