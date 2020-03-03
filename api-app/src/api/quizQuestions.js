3d9f98b181037d0054dc2530219e5150de14be4c
Andrew Seaman <andrew.seaman@mullenmail.com>
2020-03-03 12:52:15 -0800

    Added quiz functionality

---
+++ api-app/src/api/quizQuestions.js
@@ -0,0 +1,109 @@
+var quizQuestions = [
+  {
+      question: "How would you describe yourself",
+      answers: [
+          {
+              type: "Superman",
+              content: "Strong"
+          },
+          {
+              type: "Spiderman",
+              content: "Smart"
+          },
+          {
+              type: "Green Shadown",
+              content: "Villain"
+          },
+          {
+              type: "Wonder Woman",
+              content: "Hero"
+          }
+      ]
+  },
+  {
+      question: "Prefered weapon?",
+      answers: [
+          {
+              type: "Superman",
+              content: "Fists"
+          },
+          {
+              type: "Spiderman",
+              content: "Webs"
+          },
+          {
+              type: "Green Shadow",
+              content: "Green spray"
+          },
+          {
+              type: "Wonder Woman",
+              content: "Lasso of Truth"
+          }
+      ]
+  },
+  {
+      question: "Catchphrase?",
+      answers: [
+          {
+              type: "Superman",
+              content: "Justice waits for no one"
+          },
+          {
+              type: "Spiderman",
+              content: "Sling those webs dawg"
+          },
+          {
+              type: "Green Shadow",
+              content: "I will own all the real estate"
+          },
+          {
+              type: "Wonder Woman",
+              content: "I am super strong and I will destroy you"
+          }
+      ]
+  },
+  {
+      question: "Backstory?",
+      answers: [
+          {
+              type: "Superman",
+              content: "Destroyed planet"
+          },
+          {
+              type: "Spiderman",
+              content: "Dead uncle"
+          },
+          {
+              type: "Green Shadow",
+              content: "Failed magician"
+          },
+          {
+              type: "Wonder Woman",
+              content: "Grew up on an island"
+          }
+      ]
+  },
+  {
+      question: "Destiny?",
+      answers: [
+          {
+              type: "Superman",
+              content: "Turning evil"
+          },
+          {
+              type: "Spiderman",
+              content: "Turning good"
+          },
+          {
+              type: "Green Shadow",
+              content: "Fighting the good fight"
+          },
+          {
+              type: "Wonder Woman",
+              content: "Fighting the not good fight"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
--- api-app/src/questionLoader.js
+++ api-app/src/questionLoader.js
@@ -1,5 +1,17 @@
 import React from 'react';
 // import questionList from '/Users/andrewseaman/API-Project/api-app/src/question-loader.js';
 
-const loadQuestion = <h1>HERE I AM</h1>
+const loadQuestion = (props) => {
+  return (
+      <div className="">
+          <h1>{props.question}</p>
+          <p>{props.answerOne}</p>
+          <p>{props.answerTwo}</p>
+          <p>{props.questionThree}</p>
+          <p>{props.questionFour}</p>
+      </div>
+  );
+}
+
+
 export default loadQuestion;
