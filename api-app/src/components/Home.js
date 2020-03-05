de2236692cb028ea77c5d7d3eedbcb61546681bb
Tanya Nobal <tanyanobal@gmail.com>
2020-03-04 22:20:12 -0800

    edited the content on the home page

--- api-app/src/components/Home.js
+++ api-app/src/components/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
 	return (
 		<div className="container">
 			<h3>Background</h3>
-			<p>The Marvel Cinematic Universe has taken the world by storm introducing more people the marvelous world of superheros.</p>
+			<p>The Marvel Cinematic Universe has taken the world by storm, introducing more people to the marvelous world of superheroes.</p>
 			<p>Fans of the movies might be interested in getting into the comics, but because of just how many comics and characters there are, it can be overwhelming to know where to start.</p>
-			<p>Marvel More is here to help fans who want to get into comics and help them discover new characters. </p>
-			<p>Take the quiz to see what character might go along with your interests </p>
-			<p>To explore more characters, see the character page.</p>
+			<p>Marvel More is here to help fans who want to get into comics discover characters they might be interested in reading more about. </p>
+			<p>Take the quiz to see what character goes along with your interests </p>
+			<p>To explore other characters, see the character page.</p>
 
 			<Link to="/QuizPage">
 				<button type = "button">
